56667a293e11a4ae60badd508234e80f51e93ac5
Agent610 <BhavsarParthD96@Gmail.com>
2024-11-01 22:14:38 -0400

    ToggleButton is complete

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ToggleSwitch.css";
 
-// const ToggleSwitch = () => {
-//   return <div> Toggle Switch</div>;
-// };
+const ToggleSwitch = () => {
+  const [currentTemperatureUnit, handleToggleSwitchChange] = useState("C");
+  const handleChange = (e) => {
+    if (currentTemperatureUnit === "C") handleToggleSwitchChange("F");
+    if (currentTemperatureUnit === "F") handleToggleSwitchChange("C");
+  };
+  console.log(currentTemperatureUnit);
+  return (
+    <label className="ToggleSwitch">
+      <input
+        type="checkbox"
+        className="ToggleSwitch__box"
+        onChange={handleChange}
+      />
+      <span
+        className={
+          currentTemperatureUnit === "F"
+            ? "ToggleSwitch__slider ToggleSwitch__slider-F"
+            : "ToggleSwitch__slider ToggleSwitch__slider-C"
+        }
+      ></span>
+      <p
+        className={`ToggleSwitch__Temperature-F ${
+          currentTemperatureUnit === "F" && "ToggleSwitch__active"
+        }`}
+      >
+        F
+      </p>
+      <p
+        className={`ToggleSwitch__Temperature-C ${
+          currentTemperatureUnit === "C" && "ToggleSwitch__active"
+        }`}
+      >
+        C
+      </p>
+    </label>
+  );
+};
 
 export default ToggleSwitch;
