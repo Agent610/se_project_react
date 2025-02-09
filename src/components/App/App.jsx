4a8983acbbf23f69379d1947368294af26888c1d
Agent610 <BhavsarParthD96@Gmail.com>
2025-02-09 15:32:58 -0500

    Started Step 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,6 +14,7 @@ import AddItemModal from "../AddItemModal/AddItemModal";
 import api from "../../utils/api";
 import LoginModal from "../LoginModal/LoginModal";
 import RegisterModal from "../RegisterModal/RegisterModal";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
